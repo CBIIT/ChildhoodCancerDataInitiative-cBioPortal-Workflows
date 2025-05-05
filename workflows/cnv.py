b0cd41050d6298451da3286b0f731b93352f90de
bullenc <75388530+bullenc@users.noreply.github.com>
2025-05-05 13:20:16 -0500

    Update cnv.py

--- workflows/cnv.py
+++ workflows/cnv.py
@@ -247,7 +247,7 @@ def cnv_flow(bucket: str, manifest_path: str, destination_path: str, flow_type: 
 
         # download cnv files from S3
         runner_logger.info(f"Downloading cnv files from S3 bucket")
-        download_cnv(manifest_df, logger)
+        #download_cnv(manifest_df, logger)
         
         os.rename(log_filename, log_filename.replace(".log", "_"+get_time()+".log"))
 
