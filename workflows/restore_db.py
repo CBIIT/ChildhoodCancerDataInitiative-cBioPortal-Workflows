eb4a52d638c4da64e83f42b83779ed9529b833b6
bullenc <75388530+bullenc@users.noreply.github.com>
2025-05-07 13:14:32 -0500

    Update restore_db.py

--- workflows/restore_db.py
+++ workflows/restore_db.py
@@ -41,3 +41,26 @@ def restore_db(
     print(f"✅ Changed working directory to: {working_dir}")
 
     # download the dump file from S3
+    file_dl(source_bucket, sql_dump_path)
+    #check if the file exists
+    if not os.path.exists(sql_dump_path):
+        raise FileNotFoundError(f"File {sql_dump_path} does not exist.")
+    
+    print(f"✅ Downloaded dump file from S3: {sql_dump_path}")
+
+    # TODO: restore the database using the dump file
+
+    # TODO: validate the database restore
+
+    # TODO: upload the validation file(s) to S3
+    
+    # remove any files in the working directory
+    for file in os.listdir(working_dir):
+        file_path = os.path.join(working_dir, file)
+        if os.path.isfile(file_path):
+            os.remove(file_path)
+            print(f"✅ Removed file: {file_path}")
+        elif os.path.isdir(file_path):
+            os.rmdir(file_path)
+            print(f"✅ Removed directory: {file_path}")
+    print(f"✅ Removed all files in working directory: {working_dir}")
