1d40b6a351f010c8a54bc64180e67af68cf3bdff
bullenc <75388530+bullenc@users.noreply.github.com>
2025-05-12 09:44:33 -0500

    Update restore_db.py

--- workflows/restore_db.py
+++ workflows/restore_db.py
@@ -92,7 +92,7 @@ def restore_db(
         # combined the two counts into a single dataframe and check if row and col counts match
         combined_counts = dump_counts.merge(restore_counts, on="table_name", suffixes=("_dump", "_restore"))
         combined_counts["row_count_match"] = combined_counts["row_count_dump"] == combined_counts["row_count_restore"]
-        combined_counts["col_count_match"] = combined_counts["col_count_dump"] == combined_counts["col_count_restore"]
+        combined_counts["columm_count_match"] = combined_counts["column_count_dump"] == combined_counts["column_count_restore"]
         print(f"✅ Combined counts:")
         print(combined_counts)
 
