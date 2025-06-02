66a8937ab84691feab1801d1cad93e608bdbaf9e
bullenc <75388530+bullenc@users.noreply.github.com>
2025-06-02 15:49:26 -0500

    Update restore_db.py

--- workflows/restore_db.py
+++ workflows/restore_db.py
@@ -2,10 +2,11 @@
 
 import os
 import json
+import shutil
 from prefect import flow
 from src.utils import get_secret, upload_to_s3, file_dl, db_counter, get_time, restore_dump
-from typing import Literal
 import re
+from typing import Literal
 from prefect_shell import ShellOperation
 
 DropDownRunChoice = Literal["restore", "clear_working_dir"]
@@ -43,10 +44,10 @@ def restore_db(
         for file in os.listdir(working_dir):
             file_path = os.path.join(working_dir, file)
             if os.path.isfile(file_path):
-                os.remove(file_path)
+                shutil.rmtree(file_path)
                 print(f"✅ Removed file: {file_path}")
             elif os.path.isdir(file_path):
-                os.rmdir(file_path)
+                shutil.rmtree(file_path)
                 print(f"✅ Removed directory: {file_path}")
         print(f"✅ Removed all files in working directory: {working_dir}")
     
@@ -115,7 +116,7 @@ def restore_db(
 
         # combined the two counts into a single dataframe and check if row and col counts match
         combined_counts = dump_counts.merge(restore_counts, on="table_name", suffixes=("_dump", f"_{target_env_name}"))
-        combined_counts["columm_count_match"] = combined_counts["column_count_dump"] == combined_counts[f"column_count_{target_env_name}"]
+        combined_counts["column_count_match"] = combined_counts["column_count_dump"] == combined_counts[f"column_count_{target_env_name}"]
         combined_counts["row_count_match"] = combined_counts["row_count_dump"] == combined_counts[f"row_count_{target_env_name}"]
         
         print(f"✅ Combined counts:")
@@ -134,6 +135,6 @@ def restore_db(
                 os.remove(file_path)
                 print(f"✅ Removed file: {file_path}")
             elif os.path.isdir(file_path):
-                os.rmdir(file_path)
+                shutil.rmtree(file_path)
                 print(f"✅ Removed directory: {file_path}")
         print(f"✅ Removed all files in working directory: {working_dir}")
