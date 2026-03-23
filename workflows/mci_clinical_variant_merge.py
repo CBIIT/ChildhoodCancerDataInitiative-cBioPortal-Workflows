c0f4669e0546cfc510b681856e2bf9753c9ed01f
bullenc <75388530+bullenc@users.noreply.github.com>
2026-03-23 13:11:52 -0500

    Update mci_clinical_variant_merge.py

--- workflows/mci_clinical_variant_merge.py
+++ workflows/mci_clinical_variant_merge.py
@@ -109,6 +109,7 @@ def fetch_variant(row, reference_genome, retries=3) -> dict:
                         result["variant_classification"] = cons.get('variantClassification')
                         break
             
+            # if no results from transcript consequences, check for any hits in transcript consequence summaries which may have less stringent matching of HGVS strings and may be able to pull out annotation for variants that don't have exact matches in transcript consequences
             if result["hgvs_short"] is None and ann.get('transcriptConsequenceSummaries'):
                 for cons in ann.get('transcriptConsequenceSummaries', []):
                     if row.hgvs_protein != 'Not Reported':
@@ -122,6 +123,23 @@ def fetch_variant(row, reference_genome, retries=3) -> dict:
                             result["hgvs_short"] = cons.get('hgvspShort')
                             result["variant_classification"] = cons.get('variantClassification')
                             break
+            
+            # if still no results, modify coding string to check for hits
+            if result["hgvs_short"] is None and ('del' in row.hgvs_coding or 'dup' in row.hgvs_coding or 'ins' in row.hgvs_coding):
+                
+                # remove any letters from after del/dup/ins at the end of the string in the hgvs_coding and check if that matches hgvsc in any transcript consequences
+                coding_modified = row.hgvs_coding
+                if 'del' in coding_modified:
+                    coding_modified = coding_modified.split('del')[0] + 'del'
+                elif 'dup' in coding_modified:
+                    coding_modified = coding_modified.split('dup')[0] + 'dup'
+                elif 'ins' in coding_modified:
+                    coding_modified = coding_modified.split('ins')[0] + 'ins'
+                for cons in ann.get('transcriptConsequences', []):
+                    if coding_modified in cons.get('hgvsc', ''):
+                        result["hgvs_short"] = cons.get('hgvspShort')
+                        result["variant_classification"] = cons.get('variantClassification')
+                        break
 
             return result
 
@@ -196,7 +214,7 @@ def annotate_clinical_variants(clin_muts: pd.DataFrame, reference_genome) -> pd.
     logger.info(f"Combined DataFrame columns: {clin_muts.columns.tolist()}")
     
     # filter out any annotation failures
-    clin_muts = clin_muts[clin_muts["start"].notna() | clin_muts["end"].notna() | clin_muts["variant_type"].notna() | clin_muts["reference_allele"].notna() | clin_muts["variant_allele"].notna() | clin_muts["hgvs_short"].notna() | clin_muts["variant_classification"].notna()]
+    clin_muts = clin_muts[clin_muts["start"].notna() & clin_muts["end"].notna() & clin_muts["variant_type"].notna() & clin_muts["reference_allele"].notna() & clin_muts["variant_allele"].notna() & clin_muts["hgvs_short"].notna() & clin_muts["variant_classification"].notna()]
     
     # final count after anno
     ending_count = clin_muts.shape[0]
