9e41538a0660f283a1db7780dc08be1e186ee6d9
bullenc <75388530+bullenc@users.noreply.github.com>
2026-03-31 12:50:50 -0500

    Update pedmatch-clinical-vcf-workflow.py

--- workflows/pedmatch-clinical-vcf-workflow.py
+++ workflows/pedmatch-clinical-vcf-workflow.py
@@ -765,7 +765,9 @@ def pedmatch_clinical_vcf_flow(bucket: str, output_dir: str, manifest_path: str,
         af_df.loc[:, 'Start_Position'] = af_df['Start_Position'].astype(int)
         maf_df.loc[:, 'Chromosome'] = maf_df['Chromosome'].astype(int)
         af_df.loc[:, 'Chromosome'] = af_df['Chromosome'].str.replace('chr', '').astype(int)
-        maf_df = maf_df.set_index(['Chromosome', 'Start_Position']).combine_first(af_df.set_index(['Chromosome', 'Start_Position'])).reset_index()
+        # replace t_alt_count and t_ref_count in maf_df with values from af_df
+        maf_df['t_alt_count'] = maf_df.apply(lambda row: af_df[(af_df['Chromosome'] == row['Chromosome']) & (af_df['Start_Position'] == row['Start_Position'])]['t_alt_count'].values[0] if len(af_df[(af_df['Chromosome'] == row['Chromosome']) & (af_df['Start_Position'] == row['Start_Position'])]) > 0 else '', axis=1)
+        maf_df['t_ref_count'] = maf_df.apply(lambda row: af_df[(af_df['Chromosome'] == row['Chromosome']) & (af_df['Start_Position'] == row['Start_Position'])]['t_ref_count'].values[0] if len(af_df[(af_df['Chromosome'] == row['Chromosome']) & (af_df['Start_Position'] == row['Start_Position'])]) > 0 else '', axis=1)
         maf_df.to_csv(os.path.join(output_path, maf_file), sep="\t", index=False)
         
     @flow(name="add_vaf_flow", log_prints=True, task_runner=ConcurrentTaskRunner(max_workers=8))
